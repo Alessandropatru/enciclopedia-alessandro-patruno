84894585c4b9a029cd52bfc9136534ca259c59cd
patruno-alessandro <patrunoalessandro2001@gmail.com>
2024-11-29 17:40:11 +0100

    implementata navbar trasparente e la sezione header

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,31 +1,31 @@
-<nav class="navbar navbar-expand-lg NAVBAR-TRASPARENT">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="#"><i class="fa-solid fa-dragon fs-2 text-success"></i></a>
+<nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-transparent mt-3">
+    <div class="container-fluid ">
+        <a class="navbar-brand" href="#"><i class="fa-solid fa-dragon fs-2 text-success ms-3"></i></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse " id="navbarSupportedContent">
 
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 my-ul">
-                <li class="nav-item">
-                    <a class="nav-link " aria-current="page" href="#">PETS</a>
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0 me-3  ">
+                <li class="nav-item ">
+                    <a class="nav-link text-white fs-5 " aria-current="page" href="#">PETS</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">COLLEZIONI</a>
+                <li class="nav-item ">
+                    <a class="nav-link text-white fs-5" href="#">COLLEZIONI</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">ELENCHI</a>
+                <li class="nav-item ">
+                    <a class="nav-link text-white fs-5" href="#">ELENCHI</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">CHI SIAMO</a>
+                <li class="nav-item ">
+                    <a class="nav-link text-white fs-5" href="#">CHI SIAMO</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">BLOG</a>
+                <li class="nav-item ">
+                    <a class="nav-link text-white fs-5" href="#">BLOG</a>
                 </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
+                <li class="nav-item  dropdown">
+                    <a class="nav-link text-white fs-5 dropdown-toggle" href="#" role="button"
+                        data-bs-toggle="dropdown" aria-expanded="false">
                         Dropdown
                     </a>
                     <ul class="dropdown-menu">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,262 @@
-<x-layout>
+<x-layout class="">
+    <header>
+        <div class="container-fluid p-0 ">
+            <div id="carouselExampleSlidesOnly" class="carousel slide " data-bs-ride="carousel">
+                <div class="carousel-inner bg-custom ">
+                    <div class="carousel-item active">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-10  header-carousel-custom">
+                                    <h2 class="title-slide">Impara, <br> Esplora, Valuta</h2>
+                                    <p class="paragraph-header">la magnifica diversità <br> del regno animale</p>
 
+                                    <div class="row  text-center mt-5">
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "><span class="number-carousel">6350</span>
+                                                <br>
+                                                mammiferi
+                                            </p>
+                                        </div>
+                                        <div class="col-1 ">
+                                            <p class="text-white fs-5  "> <span class="number-carousel">10363</span>
+                                                <br>
+                                                uccelli
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5"> <span class="number-carousel">4469</span> <br>
+                                                rettili
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5"> <span class="number-carousel">3424</span><br>
+                                                anfibi
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "> <span class="number-carousel">4910</span>
+                                                <br> molluschi
+
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "> <span class="number-carousel">3497</span>
+                                                <br> pesci
+
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 text-number"> <span
+                                                    class="number-carousel">1227</span>
+                                                <br>insetti
+
+                                            </p>
+                                        </div>
+
+
+
+
+                                    </div>
+                                </div>
+                                <div class="col-10">
+                                    <img src="/media/Bird_1.webp" class="d-block w-100 img-custom" alt="...">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="carousel-item bg-custom">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-10 header-carousel-custom">
+                                    <h2 class="title-slide">Impara, <br> Esplora, Valuta</h2>
+                                    <p class="paragraph-header">la magnifica diversità <br> del regno animale</p>
+                                    <div class="row  text-center mt-5">
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "><span class="number-carousel">6350 </span>
+                                                <br>
+                                                mammiferi
+                                            </p>
+                                        </div>
+                                        <div class="col-1 ">
+                                            <p class="text-white fs-5  "> <span class="number-carousel">600</span>
+                                                <br>
+                                                uccelli
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5"> <span class="number-carousel">4469</span> <br>
+                                                rettili
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5"> <span class="number-carousel">3424</span><br>
+                                                anfibi
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "> <span class="number-carousel">4910</span>
+                                                <br> molluschi
+
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "> <span class="number-carousel">3497</span>
+                                                <br> pesci
+
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 text-number"> <span
+                                                    class="number-carousel">1227</span>
+                                                <br>insetti
+
+                                            </p>
+                                        </div>
+
+
+
+
+                                    </div>
+                                </div>
+                                <div class="col-10">
+                                    <img src="/media/Mammal_1.webp" class="d-block w-100 img-custom" alt="...">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="carousel-item bg-custom">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-10 header-carousel-custom">
+                                    <h2 class="title-slide">Impara, <br> Esplora, Valuta</h2>
+                                    <p class="paragraph-header">la magnifica diversità <br> del regno animale</p>
+                                    <div class="row  text-center mt-5">
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "><span class="number-carousel">63500 </span>
+                                                <br>
+                                                mammiferi
+                                            </p>
+                                        </div>
+                                        <div class="col-1 ">
+                                            <p class="text-white fs-5  "> <span class="number-carousel">10363</span>
+                                                <br>
+                                                uccelli
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5"> <span class="number-carousel">4469</span> <br>
+                                                rettili
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5"> <span class="number-carousel">3424</span><br>
+                                                anfibi
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "> <span class="number-carousel">4910</span>
+                                                <br> molluschi
+
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "> <span class="number-carousel">3497</span>
+                                                <br> pesci
+
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 text-number"> <span
+                                                    class="number-carousel">1227</span>
+                                                <br>insetti
+
+                                            </p>
+                                        </div>
+
+
+
+
+                                    </div>
+                                </div>
+                                <div class="col-10">
+                                    <img src="/media/Bird_2.webp" class="d-block w-100 img-custom" alt="...">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="carousel-item">
+                        <div class="container-fluid">
+                            <div class="row ">
+                                <div class="col-10  header-carousel-custom ">
+
+                                    <h2 class="title-slide">Impara, <br> Esplora, Valuta</h2>
+                                    <p class="paragraph-header">la magnifica diversità <br> del regno animale</p>
+                                    <div class="row  text-center mt-5">
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "><span class="number-carousel">6350 </span>
+                                                <br>
+                                                mammiferi
+                                            </p>
+                                        </div>
+                                        <div class="col-1 ">
+                                            <p class="text-white fs-5  "> <span class="number-carousel">10363</span>
+                                                <br>
+                                                uccelli
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5"> <span class="number-carousel">4469</span> <br>
+                                                rettili
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5"> <span class="number-carousel">3424</span><br>
+                                                anfibi
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "> <span class="number-carousel">4910</span>
+                                                <br> molluschi
+
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 "> <span class="number-carousel">3497</span>
+                                                <br> pesci
+
+                                            </p>
+                                        </div>
+                                        <div class="col-1">
+                                            <p class="text-white fs-5 text-number"> <span
+                                                    class="number-carousel">1227</span>
+                                                <br>insetti
+
+                                            </p>
+                                        </div>
+
+
+
+
+                                    </div>
+
+
+
+                                </div>
+                                <div class="col-10">
+                                    <img src="/media/Mammal_2.webp" class="d-block w-100 img-custom" alt="...">
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+            </div>
+
+    </header>
 </x-layout>
